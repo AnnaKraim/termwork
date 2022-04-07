c311e0bbae906ceb26a9d77064c2ede2a61110ba
Анна Крайм <annakraim313@gmail.com>
2022-04-07 17:36:39 +0300

    main

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import filedialog as fd
+import tkinter.messagebox as mb
+
+def find_mask(file_name):
+    f = open(file_name, 'rb')
+    a = f.readline()
+    a = list(map(int,a))
+    while not(a[0]==49 and a[1] == 50 and a[2] == 52 and a[3] == 48):
+        a = f.readline()
+        a = list(map(int, a))
+    a = a[4:20]
+    s = ""
+    for i in range(len(a)):
+        string = str(bin(a[i]))[2:]
+        s+="0"*(8-len(string))
+        s+=string
+    print(s)
+
+def choose_file():
+    file_name = fd.askopenfilename()
+    find_mask(file_name)
+
+root = Tk()
+root.resizable(width=False, height=False)
+text = Text(width=50, height=25)
+text.grid(columnspan=2)
+b1 = Button(text="Выбрать файл", command=choose_file)
+b1.place(x=100, y=40)
+
+root.mainloop()
