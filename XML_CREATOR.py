27becfb90e607dcbfe0bac03b93ae7232ac9eb1c
Анна Крайм <annakraim313@gmail.com>
2022-04-22 15:00:47 +0300

    release v1.0

--- XML_CREATOR.py
+++ XML_CREATOR.py
@@ -2,9 +2,6 @@ import xml.etree.ElementTree as ET
 
 def create_xml(file_name):
     top = ET.Element('start')
-    top.text = 'first text'
-
+    top.text = 'Financial transaction 1240'
     tree = ET.ElementTree(top)
     tree.write(file_name, xml_declaration=True)
-
-create_xml("final_file.xml")
--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from parsing import parser
+from XML_CREATOR import create_xml
+import os.path
 import tkinter.messagebox as mb
+
 
 def find_mask(file_name):
     f = open(file_name, 'rb')
     a = f.readline()
-    a = list(map(int,a))
-    while not(a[0]==49 and a[1] == 50 and a[2] == 52 and a[3] == 48):
+    a = list(map(int, a))
+    while not (a[0] == 49 and a[1] == 50 and a[2] == 52 and a[3] == 48):
         a = f.readline()
         a = list(map(int, a))
     a = a[4:20]
     s = ""
     for i in range(len(a)):
         string = str(bin(a[i]))[2:]
-        s+="0"*(8-len(string))
-        s+=string
+        s += "0" * (8 - len(string))
+        s += string
     f.close()
     return s
 
+
+def check_name():
+    i = 0
+    s = "final_file" + str(i) + ".xml"
+    while os.path.exists(s):
+        i += 1
+        s = "final_file" + str(i) + ".xml"
+    return s
+
+
 def choose_file():
     file_name = fd.askopenfilename()
-    mask = find_mask(file_name)
+    if len(file_name) > 0:
+        mask = find_mask(file_name)
+        name = check_name()
+        create_xml(name)
+        parser(file_name, name)
+
 
 root = Tk()
 root.resizable(width=False, height=False)
@@ -28,5 +47,4 @@ text = Text(width=50, height=25)
 text.grid(columnspan=2)
 b1 = Button(text="Выбрать файл", command=choose_file)
 b1.place(x=100, y=40)
-
 root.mainloop()
